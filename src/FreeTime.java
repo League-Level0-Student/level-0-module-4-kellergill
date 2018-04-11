63a6b3259dafccaab461efffcb7994f599e91ad4
gill.keller <gill.keller@SEMS-259-17.SEMS259.LAB>
2018-04-11 14:58:30 -0700

    i did free time for my java quiz

---
+++ src/FreeTime.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class FreeTime {
+public static void main(String[] args) {
+	String oof; JOptionPane.showInputDialog(null, "Yes");
+	if(oof=="Yes") {
+		System.out.println("happy");
+	}
+	else(oof=="Yes");
+		
+	
+	System.out.println("Wednesday");
+	
+}
+}
