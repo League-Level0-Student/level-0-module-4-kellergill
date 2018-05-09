fb6e90bc480da7992c9f4386f4085616d8061c84
gill.keller <gill.keller@SEMS-259-16.SEMS259.LAB>
2018-05-09 14:51:53 -0700

    today we did for loop gauntlet

---
+++ src/ForloopGauntlet.java
@@ -0,0 +1,54 @@
+
+public class ForloopGauntlet {
+public static void main(String[] args) {
+//	for (int i = 0; i < 101; i++) {
+//		System.out.println(i);
+//	}
+//	for (int i = 101; i > 0; i--) {
+//		System.out.println(i);
+//	}
+//	for (int i = 0; i < 101; i++) {
+//		if(i%2==0) {
+//			System.out.println(i);
+//		}
+//		for (int i = 0; i < 100; i++) {
+//			if(i%2==1) {
+//				System.out.println(i);
+//			}
+//	for (int i = 0; i < 501; i++) {
+//		if(i%2==0) {
+//			System.out.println(i + "is even");
+//		}
+//			if(i%2==1) {
+//				System.out.println(i + "is odd");
+//			}
+//			for (int i = 0; i < 778; i++) {
+//				if(i%7==0) {
+//					System.out.println(i);
+//				}
+//	int z=0;
+//	for (int i = 2005; i < 2019; i++,z++) {
+//		System.out.println("in" +  i  + "I was" +  z);
+//	}
+//	for (int i = 0; i < 3; i++) {
+//		for (int j = 0; j < 3; j++) {
+//			System.out.println(i + "" + j);
+//		}
+	for (int i = 0; i < 7; i++) {
+		//System.out.print("*");
+		for (int j = 0; j < 6; j++) {
+			//System.out.println("*");
+			System.out.println(i + "*" + j);
+		}
+	}
+		
+	}
+			}
+		
+	
+		
+
+	
+	
+
+
--- src/FreeTime.java
+++ src/FreeTime.java
@@ -3,10 +3,8 @@ import javax.swing.JOptionPane;
 public class FreeTime {
 public static void main(String[] args) {
 	String oof; JOptionPane.showInputDialog(null, "Yes");
-	if(oof=="Yes") {
-		System.out.println("happy");
-	}
-	else(oof=="Yes");
+	
+	//else(oof=="Yes");
 		
 	
 	System.out.println("Wednesday");
